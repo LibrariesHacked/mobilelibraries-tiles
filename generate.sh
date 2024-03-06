713a40c5eb3f0c12975fd7c62cf95bff858b3cbb
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-03-06 11:36:52 +0000

    Changing file name

--- generate.sh
+++
@@ -1,4 +0,0 @@
-rm -rf tiles
-mkdir tiles
-tippecanoe -r1 --no-tile-compression --drop-densest-as-needed --no-tile-size-limit --output-to-directory data/stops.geojson data/trips.geojson --force
-find . -name '*.pbf' -exec sh -c 'mv "$0" "${0%.pbf}.mvt"' {} \;
