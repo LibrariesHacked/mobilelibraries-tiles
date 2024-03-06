0f45c91861a97e0e12e34e831faf05b7b5708214
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-03-06 11:58:09 +0000

    Adding tiles directory name to tippecanoe command

--- generate_tiles.sh
+++ generate_tiles.sh
@@ -1,4 +1,4 @@
 rm -rf tiles
 mkdir tiles
-tippecanoe -r1 --no-tile-compression --drop-densest-as-needed --no-tile-size-limit --output-to-directory data/trips.geojson data/stops.geojson  --force
+tippecanoe -r1 --no-tile-compression --drop-densest-as-needed --no-tile-size-limit --output-to-directory tiles data/trips.geojson data/stops.geojson  --force
 find . -name '*.pbf' -exec sh -c 'mv "$0" "${0%.pbf}.mvt"' {} \;
